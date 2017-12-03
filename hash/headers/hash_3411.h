270f623ef59d630be68748a62f354dccc26d8ac3
kserg9707 <comservi@ya.ru>
2017-12-03 22:08:09 +0300

    minor fix: compatibility of hash and pbkdf2 with c++
    Signed-off-by: kserg9707 <comservi@ya.ru>

--- hash/headers/hash_3411.h
+++ hash/headers/hash_3411.h
@@ -3,12 +3,21 @@
 
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 uint8_t* hash_generate_256(const uint8_t* data, size_t len);
 uint8_t* hash_generate_512(const uint8_t* data, size_t len);
 uint8_t* hmac_generate_256(
         const uint8_t* key, size_t keylen, const uint8_t* data, size_t len);
 uint8_t* hmac_generate_512(
         const uint8_t* key, size_t keylen, const uint8_t* data, size_t len);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
 
