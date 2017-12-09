4dd17fb639cca8470e3179ba6804ed9c45af3e54
Maxim Anfinogenov <m.anfinogenov0@gmail.com>
2017-12-09 17:56:08 +0300

    Added simple getopt for further functionality
    Signed-off-by: Maxim Anfinogenov <m.anfinogenov0@gmail.com>

--- main.cpp
+++ main.cpp
@@ -11,6 +11,27 @@
 #include <unistd.h>
 #include <termios.h>
 #include <stdint.h>
+#include <getopt.h>
+
+struct globalargs_t {
+    bool archiver_verbose = false; // -v, --verbose
+    bool password_asterisks = false; // -s, --show
+    char* out_filename; // -o, --output
+    char** input_files; // -i, --input
+    unsigned number_of_input_files = 0;
+} global_args;
+
+static const struct option long_opts[] =
+{
+    { "verbose", no_argument, NULL, 'v' },
+    { "show", no_argument, NULL, 's' },
+    { "output", optional_argument, NULL, 'o' },
+    { "input", required_argument, NULL, 'i' },
+    { "help", no_argument, NULL, 'h'},
+    { NULL, no_argument, NULL, 0 }
+};
+
+static const char* opt_string = "vsi:o:h?";
 
 static unsigned char getch (void)
 {
@@ -45,34 +66,60 @@ static std::string get_password (const char* output, bool show_asterisk=true)
             if (password.length() != 0)
             {
                 if (show_asterisk)
-                {
                     std::cout << "\b \b";
-                }
                 password.resize(password.length()-1);
             }
         }
         else
         {
             password += temp;
-            std::cout << '*';
+            if (show_asterisk)
+                std::cout << '*';
         }
     }
     
     return password;
 }
-/*
-void usage (char** argv)
+
+void print_usage (char** argv)
 {
     std::cout << "Usage: " << argv[0] << std::endl;
     std::cout <<
-    "" << std::endl <<
-    "" << std::endl <<
+    "-s (--show): Show password symbols as * when typing" << std::endl <<
+    "-v (--verbose): Enable verbose output for archiver" << std::endl <<
     "" << std::endl;
+
+    exit(EXIT_FAILURE);
 }
-*/
-int main (/*int argc, char** argv*/)
+
+int main (int argc, char** argv)
 {
-    std::string pass = get_password("Enter password: ");
-    std::cout << std::endl << "Your super-secret password: " << pass << std::endl;
-    return 0;
+    int long_index;
+    int opt = getopt_long (argc, argv, opt_string, long_opts, &long_index);
+    while (opt != -1)
+    {
+        switch (opt)
+        {
+            case 'v':
+                global_args.archiver_verbose = true;
+                break;
+            case 's':
+                global_args.password_asterisks = true;
+                break;
+            case 'h':
+            case '?':
+                print_usage(argv);
+                break;
+            default: 
+                break;
+        }
+        opt = getopt_long (argc, argv, opt_string, long_opts, &long_index);
+    }
+
+    std::string pass = get_password("Enter password: ", global_args.password_asterisks);
+    std::cout << std::endl;
+    std::cout << "Password demo. ";
+    std::cout << "Your super-secret password: " << pass << std::endl;
+    
+    exit(EXIT_SUCCESS);
 }
