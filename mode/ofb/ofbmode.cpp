e3ce80dd36e6e7415848fb5a0bd0aa1888b5313d
Maxim Anfinogenov <m.anfinogenov0@gmail.com>
2017-12-15 08:27:32 +0300

    GOST R 34.13 working and tested OFB mode
    Signed-off-by: Maxim Anfinogenov <m.anfinogenov0@gmail.com>

--- mode/ofb/ofbmode.cpp
+++ mode/ofb/ofbmode.cpp
@@ -4,13 +4,13 @@
 #define POOL_SIZE 1024
 
 std::queue<uint8_t> pool;
-static uint8_t iv[16];
+static uint8_t iv[32];
 
 void init_gmm_generator (uint8_t* init, uint8_t* key)
 {
     GOST3412::lib_init();
     GOST3412::set_key(key);
-    std::copy(init, init+16, iv);
+    std::copy(init, init+32, iv);
 }
 
 void fin_gmm_generator (void)
@@ -22,10 +22,14 @@ static void fill_queue (void)
 {
     for (int j = 0; j < POOL_SIZE; j++) 
     {
-        GOST3412::encrypt_block(iv);
+        uint8_t tmp[16] = {0};
+        std::copy(iv+16, iv+32, tmp);
+        std::copy(iv, iv+16, iv+16);
+        GOST3412::encrypt_block(tmp);
+        std::copy(tmp, tmp+16, iv);
         for (int i = 0; i < 16; i++) 
         {
-            pool.push(iv[i]);
+            pool.push(tmp[i]);
         }
     }   
 }
@@ -39,4 +43,12 @@ uint8_t get_gmm (void)
     uint8_t gmm = pool.front();
     pool.pop();
     return gmm;
+}
+
+void encrypt_block (uint8_t* data)
+{
+    for (int i = 0; i < 16; i++)
+    {
+        data[i] ^= get_gmm();
+    }
 }
--- mode/ofb/ofbmode.hpp
+++ mode/ofb/ofbmode.hpp
@@ -5,3 +5,5 @@ void init_gmm_generator (uint8_t* init, uint8_t* key);
 void fin_gmm_generator (void);
 
 uint8_t get_gmm (void);
+
+void encrypt_block (uint8_t* data);
